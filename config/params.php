5a842f3f5d7ff925cca658fbb25756d589303143
Renne Castellanos <xan217@hotmail.com>
2021-06-10 23:22:51 -0500

    Revert "BK_0001: Database Structure defined"
    This reverts commit 6ac5769e9490c68db715f0c80104f1a2ce51db24.

--- config/params.php
+++ config/params.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'adminEmail' => '16ballcreations@gmail.com',
-    'senderEmail' => 'soporte@socialgrafo.com',
-    'senderName' => 'Soporte Socialgrafo',
+    'adminEmail' => 'admin@example.com',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
 ];
